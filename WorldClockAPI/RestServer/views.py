21a314ad45649f37a4af67084519bdc7e7606775
Douglas Freitas <dotfreitas@gmail.com>
2021-06-28 23:58:45 -0300

    REST Server working

--- WorldClockAPI/RestServer/views.py
+++ WorldClockAPI/RestServer/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
+from datetime import datetime, timezone
+
+# Create your views here.
+
+
+def index(request):
+    return HttpResponse('REST server returning a JSON like: {"currentDateTime":"2019-08-12T14:40Z"}')
+
+
+def currentDateTime(request):
+    date = datetime.now(timezone.utc).strftime("%Y-%m-%dT%H:%MZ")
+    return JsonResponse({"currentDateTime": date})
 
 # Create your views here.
